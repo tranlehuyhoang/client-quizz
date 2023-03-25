efd1a9c35bad9823212fbca8846c5bd5a77d6fdb
Huyh Huyhu <2509roblox@gmail.com>
2023-03-25 11:37:57 +0700

    firstfffa5

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <HashRouter>
+    
       <App />
-    </HashRouter>
+    
   </Provider>
 );
